872d859bf39ea5407a7af7e0281be3d43462feaa
yena kwon <ewko0220@gmail.com>
2025-07-13 11:04:22 +0900

    5차 수정
    ! 탭이 닫혀도 횟수가 초기화되지 않는 현상 재발생
    ㄴ chrome storage로 바꾸면서 일어난 에러로 파악
    ㄴ background.js 재수정: 사용한 탭 ID를 스토리지에 저장해 활성화된 탭을 파악하는 방식으로 수정

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,21 +1,21 @@
 // 크롬 확장 프로그램의 service worker (필수, 현재는 기능 없음)
 
-// 작업 중인 탭들을 추적
-let activeWorkTabs = new Set();
-
 // 팝업에서 작업 시작 시 호출되는 함수
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "start-work") {
-    activeWorkTabs.add(request.tabId);
+    // 활성 탭 ID를 스토리지에 저장
+    chrome.storage.local.set({ activeWorkTabId: request.tabId });
     sendResponse({ success: true });
   }
 });
 
 // 탭이 닫힐 때 해당 탭에서 작업했던 경우에만 초기화
 chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
-  // 해당 탭에서 작업했던 경우에만 초기화
-  if (activeWorkTabs.has(tabId)) {
-    chrome.storage.local.remove("successCount");
-    activeWorkTabs.delete(tabId);
-  }
+  // 스토리지에서 활성 탭 ID를 확인
+  chrome.storage.local.get(["activeWorkTabId"], (result) => {
+    if (result.activeWorkTabId === tabId) {
+      // 해당 탭에서 작업했던 경우에만 초기화
+      chrome.storage.local.remove(["successCount", "activeWorkTabId"]);
+    }
+  });
 });
